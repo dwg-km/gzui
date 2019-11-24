acfba58e9b4a4c434ffa1785f1f67246c184ab3f
looover <zhangruibin126@126.com>
2019-11-24 12:44:37 +0800

    complish hor code

--- cali.h
+++ cali.h
@@ -1,10 +1,176 @@
 #ifndef CALI_H
 #define	CALI_H
 
+#include <QComboBox>
+#include <QGroupBox>
+
 #include "UiTemplate.h"
 
 #include "ui_interface.h"
 #include "APIDataInterface.hpp"
+
+class LineEditGroup : public QGroupBox {
+public:
+	LineEditGroup(QString name, MECHAINE * p, QWidget *parent = NULL) 
+		: QGroupBox(parent),
+		property(p)
+	{
+		QGridLayout * horLayout = new QGridLayout;
+
+		QPushButton *left = new QPushButton(this);
+		left->setText(tr("PRINT"));
+		left->resize(72, 28);
+		left->setStyleSheet("background-color: rgb(9, 148, 220)");
+		horLayout->addWidget(left, 0, 0, 1, 1);
+
+		QString color = property->PrintColor;
+		QStringList colorList;
+		colorList = color.split(";");
+	
+		int colnum = property->PrinterColorNum;   //列数
+		int rownum = property->PrinterGroupNum; //行数
+		for(int j = 0; j < rownum; j++){
+			QLabel *label = new QLabel();
+			label->setText(QString::number(j));
+			label->setAlignment(Qt::AlignCenter);
+			horLayout->addWidget(label, j + 1, 0);
+		}
+		for(int i = 0; i < colnum; i++){
+			QLabel *label = new QLabel();
+			//label->setFixedHeight(40);
+			label->setText(colorList.at(i));
+			label->setAlignment(Qt::AlignCenter);
+			horLayout->addWidget(label, 0, i + 2, 1, 1);
+		}
+
+		for(int j = 0; j < rownum; j++){
+			for(int i = 0; i < colnum; i++){
+				QLineEdit *lineEdit = new QLineEdit(this);
+				//lineEdit->resize(40, 28);
+				horLayout->addWidget(lineEdit, j + 1, i + 2);
+				matrix.push_back(lineEdit);
+			}
+		}
+		
+		setLayout(horLayout);
+
+		//connect(left, SIGNAL(clicked()), this, SLOT(on_left_clicked()));
+	}
+	void UpdataContext(int * data){
+		int colnum = property->PrinterColorNum;   //列数
+		int rownum = property->PrinterGroupNum; //行数
+		for(int j = 0; j < rownum; j++){
+			for(int i = 0; i < colnum; i++){
+				QString text = QString::number(data[j * colnum + i]);
+				matrix[j * colnum + i]->setText(text);
+			}
+		}	
+	}
+private:
+	struct MECHAINE* property;
+	QVector<QLineEdit *> matrix;
+};
+
+class HorizontalWidget : public QWidget{
+	Q_OBJECT
+public:
+	HorizontalWidget(QWidget *parent = NULL) : QWidget(parent){
+		property = new MECHAINE;
+		GetPrinterProperty(property);
+
+		resComBox = new QComboBox;
+		speedComBox = new QComboBox;
+		gridLayout = new QGridLayout;
+
+		int resolution[8] = {0};
+		int num = LoadPrintResList(resolution);
+		if(0 != num){
+			for(int i = 0; i < num; i++){
+				resComBox->addItem(QString::number(resolution[i]));
+			}
+		}
+		res = resComBox->currentText().toInt();
+		connect(resComBox, SIGNAL(currentTextChanged(const QString&)), this, SLOT(ResChanged(const QString&)));
+		
+		speedComBox->addItem("低速");
+		speedComBox->addItem("中速");
+		speedComBox->addItem("高速");
+		speed = speedComBox->currentIndex();
+		connect(speedComBox, SIGNAL(currentIndexChanged(int)), this, SLOT(SpeedChanged(int)));
+		
+		leftGroup = new LineEditGroup("left", property, this);
+		rightGroup = new LineEditGroup("right", property, this);
+
+		gridLayout->addWidget(resComBox,	0, 1, 1, 1);
+		gridLayout->addWidget(speedComBox,	0, 2, 1, 1);
+		gridLayout->addWidget(leftGroup,	1, 0, 1, 3);
+		gridLayout->addWidget(rightGroup,	2, 0, 1, 3);
+
+		setLayout(gridLayout);
+	}
+	void UpdataData(){
+		int data[64];
+		if(LoadCalibrationParam(UI_CMD::CMD_CALI_HORIZON_LEFT, 
+			res, speed, data) <= 0){
+			memset(data, 0, sizeof(data));
+		}
+		leftGroup->UpdataContext(data);
+
+		if(LoadCalibrationParam(UI_CMD::CMD_CALI_HORIZON_RIGHT, 
+			res, speed, data)){
+			memset(data, 0, sizeof(data));
+		}
+		rightGroup->UpdataContext(data);
+	}
+	void SaveData(){
+	
+	}
+	virtual void showEvent(QShowEvent * event){
+		UpdataData();
+	}
+	virtual void hideEvent(QHideEvent * event){
+
+	}
+	void PrintLeftCali(){
+		int data[64];
+		SaveCalibrationParam(UI_CMD::CMD_CALI_HORIZON_RIGHT,
+				res, speed,  data, 0);
+
+		PrintCalibration(UI_CMD::CMD_CALI_HORIZON_RIGHT,
+				res, speed, 0);
+	}
+	void PrintRightCali(){
+		int data[64];
+		SaveCalibrationParam(UI_CMD::CMD_CALI_HORIZON_RIGHT,
+				res, speed,  data, 0);
+
+		PrintCalibration(UI_CMD::CMD_CALI_HORIZON_RIGHT,
+				res, speed, 0);
+	}
+public slots:
+	void ResChanged(const QString & text){
+		res = text.toInt();
+		qDebug() << "res changed " << res;
+		UpdataData();
+	}
+	void SpeedChanged(int index){
+		speed = index;
+		//speed = speedComBox->currentIndex();
+		qDebug() << "speed changed " << speed;
+		UpdataData();
+	}
+private:
+	struct MECHAINE* property;
+	int speed;
+	int res;
+
+	QComboBox * resComBox;
+	QComboBox * speedComBox;
+	LineEditGroup *leftGroup;
+	LineEditGroup *rightGroup;
+
+	QGridLayout * gridLayout;
+};
 
 class CaliDialog : public UiTemplate
 {
@@ -77,8 +243,7 @@ public:
 
 		//QWidget * widget = new QWidget;
 		//widgetlist->addTab(widget, "Horizontal");
-		horWidget = new QWidget;
-		LayoutHoriCali();
+		QWidget * horWidget = new HorizontalWidget;
 		widgetlist->addTab(horWidget, "Horizontal");
 	}
 	void AddBidirectonCaliWidget(){
@@ -91,87 +256,9 @@ public:
 		QWidget * widget = new QWidget;
 		widgetlist->addTab(widget, "Vertical");
 	}
-	void LayoutHoriCali()
-	{
-		QGridLayout * leftLayout = new QGridLayout;
-
-		QComboBox * rateComBox = new QComboBox;
-		QComboBox * speedComBox = new QComboBox;
-		struct MECHAINE* property = new MECHAINE;
-		GetPrinterProperty(property);
-
-		int resolution[8] = {0};
-
-		int num = LoadPrintResList(resolution);
-		if(0 != num){
-			for(int i = 0; i < num; i++){
-				rateComBox->addItem(QString::number(resolution[i]));
-			}
-		}
-		
-		speedComBox->addItem("低");
-		speedComBox->addItem("中");
-		speedComBox->addItem("高");
-
-		int y=0;
-		leftLayout->addWidget(rateComBox, y, 0);
-		leftLayout->addWidget(speedComBox, y++, 1);
-
-
-		int rownum = property->PrinterColorNum;   //列数
-		int colNum = property->PrinterGroupNum; //行数
-		
-		QString color = property->PrintColor;
-		QPushButton *left = new QPushButton(this);
-		left->setText("左校准");
-		//left->resize(72, 28);
-		//left->setStyleSheet("background-color: rgb(9, 148, 220)");
-		//connect(left, SIGNAL(clicked()), this, SLOT(on_left_clicked()));
-		//
-		QPushButton *right = new QPushButton(this);
-		right->setText("右校准");
-		//right->resize(72, 28);
-		//right->setStyleSheet("background-color: rgb(9, 148, 220)");
-		//connect(right, SIGNAL(clicked()), this, SLOT(on_right_clicked()));
-
-		QStringList colorList;
-		colorList = color.split(";");
-
-		//QRegExp reg("[a-zA-Z0-9]+$");
-		//QValidator *validator = new QRegExpValidator(reg)
-/*
-		for(int i = 0; i < rownum; i++){
-			QLabel *label = new QLabel();
-			label->setText(colorList.at(i));
-			label->setAlignment(Qt::AlignCenter);
-			leftLayout->addWidget(label, y, i + 1);
-		}
-		y++;
-*/		
-		QLabel *label = new QLabel("aaa"); 
-		qDebug()<<"label height"<<label->height();
-		label->setFixedHeight(40);
-		qDebug()<<"label height"<<label->height();
-		leftLayout->addWidget(label, y++, 2, 1, 1);
-
-		leftLayout->addWidget(left, y, 0);
-		leftLayout->addWidget(right, colNum + y, 0);
-		for(int i = 0; i < colNum * 2; i++){
-			for(int j = 0; j < rownum; j++){
-				QLineEdit *lineEdit = new QLineEdit(this);
-				//lineEdit->resize(40, 28);
-				//lineEdit->setValidator (validator);
-				leftLayout->addWidget(lineEdit, i + y, j + 1);
-				//m_lineEdit.push_back(lineEdit);
-			}
-		}
-
-		horWidget->setLayout(leftLayout);
-	}
 	
 private:
 	QTabWidget * widgetlist;
-	QWidget * horWidget;
 
 };
 
